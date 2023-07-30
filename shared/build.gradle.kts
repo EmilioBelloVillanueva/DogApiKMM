5a1add1b939e21f50a2915168cb4334987effffb
Emilio Bello <emilio.bellovillanueva@gmail.com>
2023-07-30 21:02:22 +0200

    Uptade Setup Project, Realm to 1.10.2 and Ktor engines

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
-    id("io.realm.kotlin") version "1.10.0"
+    id("io.realm.kotlin") version "1.10.2"
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
     targetHierarchy.default()
 
-    android() {
+    androidTarget () {
         compilations.all {
             kotlinOptions {
                 jvmTarget = "1.8"
@@ -27,14 +27,41 @@ kotlin {
     }
 
     sourceSets {
+        val ktorVersion = "2.3.2"
+
         val commonMain by getting {
             dependencies {
-                //put your multiplatform dependencies here
+                //MVVM Moko
+                implementation("dev.icerock.moko:mvvm-core:0.16.1")
+                //Koin DI
+                implementation("io.insert-koin:koin-core:3.4.2")
+                //commonMainApi("io.insert-koin:koin-ktor:3.4.0")
+                //Ktor
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                //Realm
+                implementation("io.realm.kotlin:library-base:1.10.0")
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
+            }
+        }
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain2 by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:$ktorVersion")
             }
         }
     }
@@ -50,19 +77,4 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-}
-
-dependencies {
-    //MVVM Moko
-    commonMainApi("dev.icerock.moko:mvvm-core:0.16.1")
-
-    //Koin DI
-    commonMainApi("io.insert-koin:koin-core:3.4.2")
-    //commonMainApi("io.insert-koin:koin-ktor:3.4.0")
-
-    //Ktor
-    commonMainApi("io.ktor:ktor-client-core:2.3.2")
-
-    //Realm
-    commonMainApi("io.realm.kotlin:library-base:1.10.0")
 }
